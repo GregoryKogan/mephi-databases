5a5787322b59b9a77152dfa6153a28c9e373fcca
Gregory Koganovskiy <grisha.koganovskiy@gmail.com>
2024-11-13 17:14:15 +0300

    Update Password model to enforce cascading delete and update on UserID

--- internal/models/password.go
+++ internal/models/password.go
@@ -6,7 +6,7 @@ import (
 
 type Password struct {
 	gorm.Model
-	UserID    uint   `gorm:"index;unique;not null"`
+	UserID    uint   `gorm:"index;unique;not null;onDelete:CASCADE;onUpdate:CASCADE"`
 	Hash      []byte `gorm:"type:bytea;not null"`
 	Salt      []byte `gorm:"type:bytea;not null"`
 	Algorithm string `gorm:"not null"`
